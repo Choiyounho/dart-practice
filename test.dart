ef595687968044d3fcd18b7c353f46581fe28858
Choiyounho <maxosa78@gmail.com>
2023-12-16 06:10:58 +0900

    feat: dart switch 문 적용

--- test.dart
+++ test.dart
@@ -7,12 +7,23 @@ void main() {
 
   print(add(10, 20));
 
-  var random = Random.secure().nextInt(100);
-  print('랜덤으로 뽑은 숫자 [$random] 이 20 보다 크다면 True 출력, 아니면 false 출력');
-  if (random > 20) {
+  var randomNumber = Random.secure().nextInt(100);
+  print('랜덤으로 뽑은 숫자 [$randomNumber] 이 20 보다 크다면 True 출력, 아니면 false 출력');
+  if (randomNumber > 20) {
     print("True");
   } else {
     print("false");
+  }
+
+  switch (randomNumber) {
+    case > 90:
+      print("스위치문 90 이상의 숫자");
+    case > 70:
+      print("스위치문 70보다 크고 90 이하인 숫자");
+    case 3:
+      print("스위치문 3");
+    default:
+      print("그 외 다른 조건들");
   }
 }
 
