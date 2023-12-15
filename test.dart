24b37b5e0507d197dbddca4e3cf0d000adb54451
Choiyounho <maxosa78@gmail.com>
2023-12-16 06:06:52 +0900

    feat: Random을 사용한 조건식 적용

--- test.dart
+++ test.dart
@@ -1,9 +1,19 @@
+import 'dart:math';
+
 void main() {
   var text = 'Hello World';
 
   print(text);
 
   print(add(10, 20));
+
+  var random = Random.secure().nextInt(100);
+  print('랜덤으로 뽑은 숫자 [$random] 이 20 보다 크다면 True 출력, 아니면 false 출력');
+  if (random > 20) {
+    print("True");
+  } else {
+    print("false");
+  }
 }
 
 int add(int num1, int num2) {
